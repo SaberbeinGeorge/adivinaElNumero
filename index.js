c1895ea8541e4a005d04dfcc11f3b8ae5faaa8d7
George Sandoval <saberbeingeorge4816@gmail.com>
2022-06-08 09:36:19 -0500

    change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const nombre = prompt('Bienvenido al pinche juego "Adivina el numero" Ingresa tu
 let intentos = 0;
 let numero;
 for (let i = 0; i < 5; intentos++ ) {
-     numero = parseInt(prompt (`ingresa tu numero de la suerte mi estimado ${nombre}`));
+     numero = parseInt(prompt (`ingresa un numero entre el 1 y 100 mi estimado ${nombre}`));
     if(numero > aleatory){
         alert('el numero que te hara ganar es menor al que ingresaste')
     } else if (numero < aleatory) {
